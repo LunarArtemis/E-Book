32fa9fb1cfe0fa9db6f5c6d76f3e2d3ce701312f
LunarArtemis <ireena51@gmail.com>
2022-10-22 00:17:47 +0700

    Test 2

--- src/state/Book1.java
+++ src/state/Book1.java
@@ -3,6 +3,9 @@ package state;
 import java.awt.Graphics2D;
 
 public class Book1 extends BookState {
+    public static void init(){
+
+    }
     public Book1(StateManager sm) {
         super(sm);
     }
